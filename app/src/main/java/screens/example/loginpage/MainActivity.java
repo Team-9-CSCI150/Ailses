da6bba5d3134d343b79ed22e5dc3e875037c4592
Hassan1 <halsharif@mail.fresnostate.edu>
2019-10-31 16:06:58 -0700

    button 1

--- app/src/main/java/screens/example/loginpage/MainActivity.java
+++ app/src/main/java/screens/example/loginpage/MainActivity.java
@@ -1,17 +1,31 @@
 package screens.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // add logic here....
+        Button getStarted =  findViewById(R.id.getStarted);
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                System.out.println("Button Clicked");
 
-        // another comment here
-        // another comment
+            Intent activityLocationIntent = new Intent(getApplicationContext(),LocationActivity.class);
+                    startActivity(activityLocationIntent);
+            }
+        });
     }
 }
