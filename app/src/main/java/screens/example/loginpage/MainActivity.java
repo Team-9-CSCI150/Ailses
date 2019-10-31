370ec9283d6517c809cd21b81a81fb60ac802bc8
Iraj Saghir <irajsaghir2@gmail.com>
2019-10-31 14:50:46 -0700

    commit 1

--- app/src/main/java/screens/example/loginpage/MainActivity.java
+++ app/src/main/java/screens/example/loginpage/MainActivity.java
@@ -1,15 +1,34 @@
 package screens.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getStarted = findViewById(R.id.getStarted);
+
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to new screen
+Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+startActivity(intent);
+            }
+        });
         // add logic here....
         // another comment
     }
+
+
 }
