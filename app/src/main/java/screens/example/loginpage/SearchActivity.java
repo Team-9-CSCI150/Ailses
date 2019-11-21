4ac30eef9296ccfad78b28ce8fea87939d11b6a0
Iraj Saghir <irajsaghir2@gmail.com>
2019-11-21 15:30:52 -0800

    search activity

--- app/src/main/java/screens/example/loginpage/SearchActivity.java
+++ app/src/main/java/screens/example/loginpage/SearchActivity.java
@@ -1,8 +1,11 @@
 package screens.example.loginpage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.action_menu){
+
+        }
+
+        if(id == R.id.action_help){
+
+        }
+
+        if(id == R.id.action_settings){
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
